98dcbf8e46911545997f202ab19ca3ebe3070258
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-17 11:29:28 +0700

    Add a feature to keep the sampled noise constant per epoch
    It works similarly to how the `shuffle` argument works for
    `torch.utils.data.DataLoader`.
    
    This is useful for evaluation.

--- src/models/data_loader.py
+++ src/models/data_loader.py
@@ -13,6 +13,8 @@ class NoiseAugmentLoader:
         snr_dataset: MSSNSDataset,
         rir_dataset: FLAIRDataset | None = None,
         snr_prob: float | None = None,
+        *,
+        resample: bool = True
     ) -> None:
         if snr_prob is not None and not 0 < snr_prob < 1:
             raise ValueError("snr_prob must be between 0 and 1")
@@ -30,6 +32,8 @@ class NoiseAugmentLoader:
             len(self._snr_dataset) / (len(self._rir_dataset) + len(self._snr_dataset))
             if snr_prob is None else snr_prob
         )
+
+        self._cache = None if resample else [None] * len(self._data_loader)
 
     def _apply_snr_noise(self, samples, snr_mask):
         snr_noises, snr_levels = self._snr_dataset.get_samples(
@@ -49,31 +53,46 @@ class NoiseAugmentLoader:
             samples[rir_mask], rir_samples
         )[..., :samples.shape[-1]].type_as(samples)
 
+        dims = tuple(range(1, samples.ndim))
+        clean_rms = (samples ** 2).mean(dims, True).sqrt() + eps
+        noisy_rms = (noisy ** 2).mean(dims, True).sqrt() + eps
+
+        return rir_samples, noisy * (clean_rms / noisy_rms)
+
+    def _augment(self, batch):
+        samples, *other = batch if isinstance(batch, tuple) else (batch, ())
+        batch_size = samples.shape[0]
+
+        snr_mask = torch.bernoulli(
+            torch.full((batch_size,), self._snr_prob),
+            generator=self._generator
+        ).bool()
+        noisy_samples = torch.empty(samples.shape, dtype=samples.dtype)
+
+        snr_noises, snr_levels, noisy = self._apply_snr_noise(samples, snr_mask)
+        noisy_samples[snr_mask] = noisy
+
+        rir_samples, noisy = self._apply_rir_noise(samples, ~snr_mask)
+        noisy_samples[~snr_mask] = noisy
+
+        noise_dict = {
+            "snr": snr_noises,
+            "rir": rir_samples,
+            "snr_mask": snr_mask,
+            "snr_levels": snr_levels
+        }
+
+        return samples, noisy_samples, noise_dict, *other
+
     def __len__(self):
         return len(self._data_loader)
 
     def __iter__(self):
-        for batch in self._data_loader:
-            samples, *other = batch if isinstance(batch, tuple) else (batch, ())
-            batch_size = samples.shape[0]
+        for idx, batch in enumerate(self._data_loader):
+            if self._cache:
+                if not self._cache[idx]:
+                    self._cache[idx] = self._augment(batch)
 
-            snr_mask = torch.bernoulli(
-                torch.full((batch_size,), self._snr_prob),
-                generator=self._generator
-            ).bool()
-            noisy_samples = torch.empty(samples.shape, dtype=samples.dtype)
-
-            snr_noises, snr_levels, noisy = self._apply_snr_noise(samples, snr_mask)
-            noisy_samples[snr_mask] = noisy
-
-            rir_samples, noisy = self._apply_rir_noise(samples, ~snr_mask)
-            noisy_samples[~snr_mask] = noisy
-
-            noise_dict = {
-                "snr": snr_noises,
-                "rir": rir_samples,
-                "snr_mask": snr_mask,
-                "snr_levels": snr_levels
-            }
-
-            yield samples, noisy_samples, noise_dict, *other
+                yield self._cache[idx]
+            else:
+                yield self._augment(batch)
