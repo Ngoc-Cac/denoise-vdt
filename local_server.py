753869b34354a63dd4d6855c04626d0db625bf05
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-21 16:38:25 +0700

    Refactor the main script to run API server
    Remove some unnecessary guard clauses and logging.

--- local_server.py
+++ local_server.py
@@ -1,5 +1,3 @@
-import os
-
 from src.api.app import create_app
 from src.api.logger import get_logger
 
@@ -9,12 +7,8 @@ logger = get_logger(
     rotating_file_handler=True
 )
 
-logger.info("Application has fully started. Current env %s", os.environ)
-
 app = create_app()
 
-
-if __name__ == "__main__":
-    logger.info("Starting application")
-    app.debug = True
-    app.run(host="0.0.0.0", port="8080", threaded=True)
+logger.info("Starting application")
+app.debug = True
+app.run(host="0.0.0.0", port="8080", threaded=True)
